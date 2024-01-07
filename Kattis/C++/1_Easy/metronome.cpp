c765b203b4c8f8eaf0ee7653bd11097fe7737e9f
Ayub Ali <yobo.muhammed@gmail.com>
2024-01-07 00:25:56 -0700

    updated metronome solution prescion

--- Kattis/C++/1_Easy/metronome.cpp
+++ Kattis/C++/1_Easy/metronome.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <iomanip>  
 using namespace std;
 
 int main() {
     int ticks;
-    
+
     cin >> ticks;
 
-    cout << ticks / 4.0 << endl;
+    cout << fixed << setprecision(2); 
 
+    cout << ticks / 4.0 << endl;
+    
     return 0;
 }
