e50045ae2f0f028cc6a4e4c41d132234df8efc95
Ayub Ali <yobo.muhammed@gmail.com>
2024-01-08 00:07:17 -0700

    yesterdays evening problem

---
+++ Kattis/C++/1_Easy/cold.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <algorithm>
+using namespace std; 
+
+int main() 
+{ 
+    int numtemps; 
+    int count = 0;
+
+    cin >> numtemps;
+
+    int temp[numtemps]; 
+
+    for (int i = 0; i < numtemps; i++) { 
+        cin >> temp[i]; 
+    
+        if (temp[i] < 0) {
+            count++;
+        } 
+    } 
+    cout << count << endl;
+
+    return 0; 
+}
