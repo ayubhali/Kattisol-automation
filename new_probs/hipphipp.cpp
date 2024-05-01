1d30bba89b8244a95d4668feac92cf0acbf1f731
Ayub <ayub2@ualberta.ca>
2024-05-01 06:43:01 -0600

    Auto update of problem solutions

---
+++ new_probs/hipphipp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string hipp;
+
+  hipp = "Hipp hipp hurra!";
+
+  for (int i; i < 20; i++) {
+    cout << hipp << endl;
+  }
+
+    return 0;
+}
