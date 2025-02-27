f3a5762e1b1b93d6ed7b80e974229a9851a79850
Ayub Ali <pltsummary@gmail.com>
2025-02-27 09:39:56 -0700

    Auto update of problem solutions

---
+++ kattis/python/1_Easy/millifaersla.py
@@ -0,0 +1,14 @@
+# first line int a transactoipnal fee on monnei
+# second line conssists of an integer b transaction fal feel 
+# third line integer c fee on dolladbijolla
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a < b  and a < c:
+  print("Monnei")
+elif b < a and  b < c:
+  print("Fjee")
+else:
+  print("Dolladollabilljoll")
