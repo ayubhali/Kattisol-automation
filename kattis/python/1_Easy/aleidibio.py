cda8782aeee91e03a6c315917a679f93a6019b5a
Ayub Ali <pltsummary@gmail.com>
2025-05-20 10:39:36 -0600

    Auto update of problem solutions

--- kattis/python/1_Easy/aleidibio.py
+++ kattis/python/1_Easy/aleidibio.py
@@ -2,7 +2,6 @@
 # Hannes not sure when to leave 
 # Hannes at his house -> Arnar must pick up 
 
-
 # Takes a minutes to drive to Arnar 
 # Driving to cinema will be b minutes 
 # Film starts on the minute c of the day? what minute of the day hannes should drive so they dont miss
@@ -10,11 +9,13 @@
 # Minute of the day -> 1335 -> 10:25pm time it starts the sum of the time he has to go pick up arnar then drrive then subtracted by the time it starts
 
 a = int(input())
+
 b = int(input())
+
 c = int(input())
 
 d  = a + b 
 
-e = c - d
+f = c - d
 
-print(e)
+print(f)
